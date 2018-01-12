25b4d3d84560d71a70684c749ce9cefee815cafa
Fiona Lapham <fionadlapham@gmail.com>
2018-01-12 17:15:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
-    current_value = value
+    current_value = nil
+    current_key= nil
   if value < current_value
     smallest_key = key
     smallest key
